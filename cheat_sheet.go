292c6fb643cbb01a9f985dc430d51719929203ed
Yash Sethiya <yashsethiya97@gmail.com>
2021-05-19 21:39:34 +0530

    Create cheat_sheet.go

---
+++ cheat_sheet.go
@@ -0,0 +1,13 @@
+
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Use Math package to get the max int and min int we have MaxInt64 also 
+const maxInt = math.MaxInt32  
+const minInt = math.MinInt32
+
