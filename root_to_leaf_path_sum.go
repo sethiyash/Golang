c64fbbc322e68f88937f51e7cbd7783001e080ee
Yash Sethiya <yashsethiya97@gmail.com>
2021-05-20 17:16:57 +0530

    Create root_to_leaf_path_sum.go

---
+++ root_to_leaf_path_sum.go
@@ -0,0 +1,73 @@
+//Let say you have a tree. you need to return a list which will contain all the  root to leaf path sum.
+//                1
+//        2               3
+
+// 4            5    6         7
+
+//result:= [7,8,10,11]
+
+package main
+
+import (
+    "fmt"
+)
+
+
+type Node struct {
+    data int
+    left *Node
+    right *Node
+}
+
+func newNode(input int) *Node {
+    return &Node{
+        data: input,
+    }
+}
+
+
+func RootToLeafSum(node *Node, sum int) []int {
+    result := make([]int, 0)
+    if node == nil {
+        return result
+    } else {
+        sum += node.data
+        
+        if node.left == nil && node.right == nil {
+            result = append(result, sum)
+        }
+        
+        
+        
+        leftsum := RootToLeafSum(node.left, sum)
+        result = append(result, leftsum...)
+        
+        
+        rightSum := RootToLeafSum(node.right, sum)
+        result = append(result, rightSum...)
+        
+    }
+    
+    return result
+}
+
+func main() {
+    
+    root := newNode(1)
+    
+    root.left = newNode(2)
+    root.right = newNode(3)
+    
+    root.left.left = newNode(4)
+    root.left.right = newNode(5)
+    
+    root.right.left = newNode(6)
+    root.right.right = newNode(7)
+    
+    
+    result := RootToLeafSum(root, 0)
+    
+    fmt.Println(result)
+}
+
+
